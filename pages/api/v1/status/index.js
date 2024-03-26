044ab8cdc701a03080360179330ad1c1046e731b
Vitor Scabbia <48934352+vscabbia@users.noreply.github.com>
2024-03-26 16:28:29 +0000

    finaliza o endpoint de status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,32 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ status: 200 });
+  const updatedAt = new Date().toISOString();
+  const postgresVersion = (await database.query("SHOW server_version")).rows[0]
+    .server_version;
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  const postgresOpenedConn = (
+    await database.query({
+      text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+      values: [databaseName],
+    })
+  ).rows[0].count;
+
+  const postgresMaxConn = (await database.query("SHOW max_connections;"))
+    .rows[0].max_connections;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: postgresVersion,
+        open_connections: postgresOpenedConn,
+        max_connections: parseInt(postgresMaxConn),
+      },
+    },
+  });
 }
 
 export default status;
