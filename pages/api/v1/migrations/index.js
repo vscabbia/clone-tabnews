0f21f682c734b882065c4eedcd847017950d41d6
Vitor Scabbia <48934352+vscabbia@users.noreply.github.com>
2025-05-23 22:51:35 +0000

    refactor: make `/api/v1/migrations` use `migrator` model

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,7 +1,5 @@
 import { createRouter } from "next-connect";
-import migrationRunner from "node-pg-migrate";
-import { resolve } from "node:path";
-import database from "infra/database";
+import migrator from "models/migrator";
 import controller from "infra/controller";
 
 const router = createRouter();
@@ -12,56 +10,17 @@ router.post(postHandler);
 export default router.handler(controller.errorHandlers);
 
 async function getHandler(request, response) {
-  let dbClient;
+  const pendingMigrations = await migrator.listPendingMigrations();
 
-  try {
-    dbClient = await database.getNewClient();
-
-    const defaultMigrationOptions = await getMigrationOptions(dbClient);
-
-    const pendingMigrations = await migrationRunner({
-      ...defaultMigrationOptions,
-      dryRun: true,
-    });
-
-    return response.status(200).json(pendingMigrations);
-  } finally {
-    await dbClient.end();
-  }
+  return response.status(200).json(pendingMigrations);
 }
 
 async function postHandler(request, response) {
-  let dbClient;
+  const migratedMigrations = await migrator.runPendingMigrations();
 
-  try {
-    dbClient = await database.getNewClient();
-
-    const defaultMigrationOptions = await getMigrationOptions(dbClient);
-
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrationOptions,
-      dryRun: false,
-    });
-
-    if (migratedMigrations.length > 0) {
-      return response.status(201).json(migratedMigrations);
-    }
-
-    return response.status(200).json(migratedMigrations);
-  } finally {
-    await dbClient.end();
+  if (migratedMigrations.length > 0) {
+    return response.status(201).json(migratedMigrations);
   }
-}
 
-async function getMigrationOptions(dbClient) {
-  const defaultMigrationOptions = {
-    dbClient: dbClient,
-    databaseUrl: process.env.DATABASE_URL,
-    dir: resolve("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pg_migrations",
-  };
-
-  return defaultMigrationOptions;
+  return response.status(200).json(migratedMigrations);
 }
