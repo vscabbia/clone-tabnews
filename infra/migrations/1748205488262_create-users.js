bc13444e5d9bc6a5afa4934bb72c417f4b75ee01
Vitor Scabbia <48934352+vscabbia@users.noreply.github.com>
2025-05-25 21:22:51 +0000

    feat: add migration to create `users` table

---
+++ infra/migrations/1748205488262_create-users.js
@@ -0,0 +1,35 @@
+exports.up = (pgm) => {
+  pgm.createTable("users", {
+    id: {
+      type: "uuid",
+      primaryKey: true,
+      default: pgm.func("gen_random_uuid()"),
+    },
+    // For reference, Github limits usernames to 39 characters.
+    username: {
+      type: "varchar(30)",
+      notNull: true,
+      unique: true,
+    },
+    // 254 characters is the max length of an e-mail
+    email: {
+      type: "varchar(254)",
+      notNull: true,
+      unique: true,
+    },
+    password: {
+      type: "varchar(72)",
+      notNull: true,
+    },
+    created_at: {
+      type: "timestamptz",
+      default: pgm.func("now()"),
+    },
+    updated_at: {
+      type: "timestamptz",
+      default: pgm.func("now()"),
+    },
+  });
+};
+
+exports.down = false;
