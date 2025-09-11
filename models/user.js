693a3bf20d2b0feb83bbeb7ddd13684ee166df84
Vitor Scabbia <48934352+vscabbia@users.noreply.github.com>
2025-09-11 15:21:20 +0000

    feat: create `user.findOneByUsername` method and `/api/v1/users/[username]` endpoint

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 import database from "infra/database.js";
-import { ValidationError } from "infra/errors.js";
+import { ValidationError, NotFoundError } from "infra/errors.js";
 
 async function create(userInputValues) {
   await validadeUniqueEmail(userInputValues.email);
@@ -68,8 +68,40 @@ async function create(userInputValues) {
   }
 }
 
+async function findOneByUsername(username) {
+  const userFound = await runSelectQuery(username);
+
+  return userFound;
+
+  async function runSelectQuery(username) {
+    const results = await database.query({
+      text: `
+      SELECT 
+        *
+      FROM
+        users
+      WHERE
+        LOWER(username) = LOWER($1)
+      LIMIT 
+        1
+      ;`,
+      values: [username],
+    });
+
+    if (results.rowCount === 0) {
+      throw new NotFoundError({
+        message: "O usuário informado não foi encontrado.",
+        action: "Verifique se o username informado está correto.",
+      });
+    }
+
+    return results.rows[0];
+  }
+}
+
 const user = {
   create,
+  findOneByUsername,
 };
 
 export default user;
