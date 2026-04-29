ef0b1f4deebe297c752288be89d10ed5c7888358
Vitor Scabbia <48934352+vscabbia@users.noreply.github.com>
2026-04-29 23:59:34 +0000

    feat: start creating sessions endpoint

--- models/user.js
+++ models/user.js
@@ -151,9 +151,41 @@ async function findOneByUsername(username) {
   }
 }
 
+async function findOneByEmail(email) {
+  const userFound = await runSelectQuery(email);
+
+  return userFound;
+
+  async function runSelectQuery(email) {
+    const results = await database.query({
+      text: `
+      SELECT 
+        *
+      FROM
+        users
+      WHERE
+        LOWER(email) = LOWER($1)
+      LIMIT 
+        1
+      ;`,
+      values: [email],
+    });
+
+    if (results.rowCount === 0) {
+      throw new NotFoundError({
+        message: "O usuário informado não foi encontrado.",
+        action: "Verifique se o email informado está correto.",
+      });
+    }
+
+    return results.rows[0];
+  }
+}
+
 const user = {
   create,
   findOneByUsername,
+  findOneByEmail,
   update,
 };
 
